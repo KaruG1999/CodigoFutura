ebc4136600694e324c7d188ee757a777175ada1e
Karen Giannetto <karengiannetto99@gmail.com>
2025-10-18 01:03:20 -0300

    Solución 2 a errores de fn hello y test

--- Clase4-Rust-Avanz/proyectos-soroban/hello-tiburona/contracts/hello-tiburona/src/lib.rs
+++ Clase4-Rust-Avanz/proyectos-soroban/hello-tiburona/contracts/hello-tiburona/src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 use soroban_sdk::{
     contract, contractimpl, contracterror, contracttype,
-    Env, Symbol, Address, String
+    Env, Symbol, Address 
 };  
 
 // Usar wee_alloc para optimizar el uso de memoria (Error en consola)
@@ -61,22 +61,24 @@ impl HelloContract {
     pub fn hello(
         env: Env,
         usuario: Address,
-        nombre: String  // Cambio temporal por String
+        nombre: Symbol  
     ) -> Result<Symbol, Error> {
         usuario.require_auth(); // Requiere que el usuario firme la transacción
 
-        // Validación del nombre (no vacio) -> Sale error con to-string 
+        // Validación del nombre (no vacio) -> Sale error con tostring 
         // let nombre_str= nombre.to_string(); 
 
-        if nombre.len() == 0 {
+        // if nombre_str.len() == 0 -> Error: función len() no disponible para Symbol
+        if nombre == Symbol::new(&env, "") {
             return Err(Error::NombreVacio);
         } 
         
         // Validación del nombre (no muy largo) -> Sale error si es muy largo
-        if nombre.len() > 32 {
+        /* if nombre.len() > 32 {
             return Err(Error::NombreMuyLargo);
         }
 
+ */
         // Incrementar el contador de saludos -> Lee el contador, lo incrementa y lo guarda
         let key_contador = DataKey::ContadorSaludos;
 
@@ -103,7 +105,7 @@ impl HelloContract {
             .instance()
             .extend_ttl(100, 100);
 
-        // Retornar el saludo -> "Hola, {nombre}"
+        // Retornar el saludo 
         Ok(Symbol::new(&env, "Hola"))
 
     }
@@ -154,13 +156,14 @@ impl HelloContract {
 #[cfg(test)]
 mod test {
     use super::*;
+    // tira error
     use soroban_sdk::Env ;
     use soroban_sdk::testutils::Address as TestAddress;
 
     #[test]
     fn test_initialize() {
         let env = Env::default();
-        let contract_id = env.register_contract(None, HelloContract);
+        let contract_id = env.register(HelloContract, ());
         let client = HelloContractClient::new(&env, &contract_id);
         
         let admin = Address::generate(&env); // funcion generate no asociada a Address
@@ -177,7 +180,7 @@ mod test {
     #[should_panic(expected = "NoInicializado")]
     fn test_no_reinicializar() {
         let env = Env::default();
-        let contract_id = env.register_contract(None, HelloContract);
+        let contract_id = env.register(HelloContract, ());
         let client = HelloContractClient::new(&env, &contract_id);
         
         let admin = Address::generate(&env);
@@ -188,10 +191,11 @@ mod test {
 
 
 
-     #[test]
+    #[test]
     fn test_hello_exitoso() {
         let env = Env::default();
-        let contract_id = env.register_contract(None, HelloContract);
+        // register_contract Deprecated -> Soluciono con register 
+        let contract_id = env.register(HelloContract, ()); 
         let client = HelloContractClient::new(&env, &contract_id);
         
         let admin = Address::generate(&env);
@@ -200,7 +204,7 @@ mod test {
         client.initialize(&admin);
         
         let nombre = Symbol::new(&env, "Ana");
-        let resultado = client.hello(&usuario, &nombre);
+        let resultado = client.hello(&usuario, &nombre); // Acá arrastra error de fn hello
         
         assert_eq!(resultado, Symbol::new(&env, "Hola"));
         assert_eq!(client.get_contador(), 1);
@@ -214,7 +218,7 @@ mod test {
     #[should_panic(expected = "NombreVacio")]
     fn test_nombre_vacio() {
         let env = Env::default();
-        let contract_id = env.register_contract(None, HelloContract);
+        let contract_id = env.register(HelloContract, ());
         let client = HelloContractClient::new(&env, &contract_id);
         
         let admin = Address::generate(&env);
@@ -222,7 +226,7 @@ mod test {
         
         client.initialize(&admin);
         
-        let vacio = Symbol::new(&env, "");
+        let vacio = Symbol::new(&env, ""); // Error de fn hello
         client.hello(&usuario, &vacio);  // Debe fallar
     }
 
@@ -231,11 +235,11 @@ mod test {
         #[test]
     fn test_reset_solo_admin() {
         let env = Env::default();
-        let contract_id = env.register_contract(None, HelloContract);
+        let contract_id = env.register(HelloContract, ());
         let client = HelloContractClient::new(&env, &contract_id);
         
         let admin = Address::generate(&env);
-        let otro = Address::generate(&env);
+        let _otro = Address::generate(&env);
         let usuario = Address::generate(&env);
         
         client.initialize(&admin);
@@ -255,7 +259,7 @@ mod test {
     #[should_panic(expected = "NoAutorizado")]
     fn test_reset_no_autorizado() {
         let env = Env::default();
-        let contract_id = env.register_contract(None, HelloContract);
+        let contract_id = env.register(HelloContract, ());
         let client = HelloContractClient::new(&env, &contract_id);
         
         let admin = Address::generate(&env);
